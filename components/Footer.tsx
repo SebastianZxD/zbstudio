53643a4a9e1643a58a1d7bbd0d0c971ad33104ca
Sebastián Zambrano <sebastianzambrano@Sebastians-MacBook-Air.local>
2024-09-19 17:40:02 -0500

    Added key for Footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,8 +25,8 @@ const Footer = () => {
         <div className='flex mt-16 md:flex-row flex-col justify-between md:justify-around items-center w-full gap-4 lg:gap-0'>
           <p className='md:text-base text-sm md:font-normal font-light'>Copyright © 2024 ZB Studio/Sebastián Zambrano</p>
           <div className='flex items-center md:gap-3 gap-6'>
-            {socialMedia.map((profile) => (
-              <a href={profile.link}>
+            {socialMedia.map((profile, idx) => (
+              <a href={profile.link} key={idx}>
                 <div className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300' key={profile.id}>
                   <img src={profile.img} alt='social' width={20} height={20}/>
                 </div>
