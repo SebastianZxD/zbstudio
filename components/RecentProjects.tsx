9d48732f857e5ae900b443ce4e24479978cd3b6c
Sebastián Zambrano <juanzambrano97@hotmail.com>
2024-11-25 18:20:08 -0500

    bug: Fixed Recent Projects overlaping in 2xl screens

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -12,7 +12,7 @@ const RecentProjects = () => {
         A small selection of {' '}
         <span className='text-purple'>recent projects</span>
       </h1>
-      <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10'>
+      <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 2xl:gap-y-32 mt-10'>
         {projects.map(({
           id,
           title,
