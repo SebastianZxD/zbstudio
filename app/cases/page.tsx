11ddbd8987e979499a2ce244762a5d6c43131fa7
Sebastián Zambrano <juanzambrano97@hotmail.com>
2024-11-27 12:54:43 -0500

    feat: Added responsiveness for the cases page, also improved visuals for study cases

--- app/cases/page.tsx
+++ app/cases/page.tsx
@@ -6,7 +6,7 @@ import { cases } from "@/data";
 
 const CasesPage = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5 md:h-screen">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5 lg:h-screen">
       <div className="max-w-7xl w-full">
         <div>
           <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen z-0' fill='white'/>
@@ -14,8 +14,8 @@ const CasesPage = () => {
           <Spotlight className='top-28 left-80 h-[80vh] w-[50vh] z-0' fill='purple'/>
           <Spotlight className='top-28 left-[80%] h-[80vh] w-[50vh] z-0' fill='white'/>
         </div>
-        <h1 className="text-4xl font-bold pt-4 md:pt-0 pb-0 md:pb-8" >Case Studies</h1>
-        <div className="flex flex-col md:flex-row gap-4 w-full justify-between items-center">
+        <h1 className="text-4xl font-bold pt-4 lg:pt-0 pb-4 lg:pb-8 text-center lg:text-start" >Case Studies</h1>
+        <div className="flex flex-col lg:flex-row gap-4 w-full justify-between items-center">
           {cases.map((caseStudy) => (
             <CaseCard key={caseStudy.id} {...caseStudy} />
           ))}
--- components/CaseCard.tsx
+++ components/CaseCard.tsx
@@ -17,7 +17,7 @@ export const CaseCard = ({ id, title, description, imageUrl }: CaseCardProps) =>
 
   if (isUnderConstruction) {
     return (
-      <CardContainer className="w-auto max-w-[300px]">
+      <CardContainer className="w-[400px] lg:max-w-[350px]">
         <CardBody className=" backdrop-blur-md relative group/card hover:shadow-2xl hover:shadow-emerald-500/[0.1] bg-black/30 border-white/[0.2] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
           <CardItem
             translateZ="50"
@@ -73,10 +73,10 @@ export const CaseCard = ({ id, title, description, imageUrl }: CaseCardProps) =>
   }
 
   return (
-    <BackgroundGradient className="border-black border-2 hover:border-slate-600">
+    <BackgroundGradient className="border-black border-2 hover:border-white rounded-xl">
       <Link href={`/cases/${id}`}>
-        <CardContainer className="w-auto max-w-[300px]">
-          <CardBody className="relative group/card hover:shadow-2xl hover:shadow-emerald-500/[0.1] bg-black w-auto sm:w-[30rem] h-auto hover:rounded-xl rounded-none p-6 border-none">
+        <CardContainer className="w-[400px] lg:max-w-[350px]">
+          <CardBody className="relative group/card hover:shadow-2xl hover:shadow-emerald-500/[0.1] bg-black w-auto sm:w-[30rem] h-auto rounded-[10px] p-6 border-none">
             <CardItem
               translateZ="50"
               className="text-xl font-bold text-white"
@@ -107,7 +107,7 @@ export const CaseCard = ({ id, title, description, imageUrl }: CaseCardProps) =>
             <div className="flex justify-between items-center mt-20">
               <CardItem
                 translateZ={20}
-                translateX={-40}
+                translateX={-30}
                 as="button"
                 className="px-4 py-2 rounded-xl text-xs font-normal text-white"
               >
